efe999ab0fdfb1806366a1459d9a57e56fcccaf6
Roman <toxi@ua.fm>
2023-01-27 16:05:32 +0200

    Update process.sh

--- config/_zaborona_v2/root/zaborona-vpn/process.sh
+++ config/_zaborona_v2/root/zaborona-vpn/process.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 set -e
 
-cp result/knot-aliases-alt.conf /etc/knot-resolver/knot-aliases-alt.conf
-systemctl restart kresd@1.service
+#cp result/knot-aliases-alt.conf /etc/knot-resolver/knot-aliases-alt.conf
+#systemctl restart kresd@1.service
+
+cp result/dnsmasq-aliases-alt.conf /etc/dnsmasq.d/zaborona-dns-resovler
+systemctl restart dnsmasq
 
 cp result/openvpn-blocked-ranges.txt /etc/openvpn/server/ccd/DEFAULT
-iptables -F azvpnwhitelist
+
+iptables -F zbrnhlpvpnwhitelist
+
+echo "" > result/openvpn-blocked-ranges-ferm-whitelist.conf
+echo "# dummy file. Filled by zaboronahelp script." >> result/openvpn-blocked-ranges-ferm-whitelist.conf
+echo "@def $WHITELIST = (" >> result/openvpn-blocked-ranges-ferm-whitelist.conf
+
 while read -r line
 do
-    iptables -w -A azvpnwhitelist -d "$line" -j ACCEPT
+    iptables -w -A zbrnhlpvpnwhitelist -d "$line" -j ACCEPT
+    echo "$line" >> result/openvpn-blocked-ranges-ferm-whitelist.conf
+
 done < result/blocked-ranges.txt
+
+echo ");" >> result/openvpn-blocked-ranges-ferm-whitelist.conf
 
 exit 0
