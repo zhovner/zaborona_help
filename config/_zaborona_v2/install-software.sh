7e76c8d195eef17a98953f4cd980ab2a511d2b00
Roman <toxi@ua.fm>
2023-01-27 17:41:53 +0200

    Update install-software.sh

--- config/_zaborona_v2/install-software.sh
+++ config/_zaborona_v2/install-software.sh
@@ -2,5 +2,5 @@
 
 # Installing software for the correct work of the resolver ( Устанавливаем софт для корректрой работы резолвера )
 echo "Installing software for the correct work of the resolver ( Устанавливаем софт для корректрой работы резолвера )"
-apt install curl pip python3 dnsmasq htop iftop net-tools git openvpn ferm
+apt install curl pip python3 dnsmasq htop iftop net-tools git openvpn ferm idn
 
