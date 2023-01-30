cd9173646b2a5503d01cf0088f4bb447a0e0fada
Roman <toxi@ua.fm>
2023-01-30 22:54:22 +0200

    Update

--- config/_zaborona_v2/root/zaborona-vpn/iptables-custom.sh
+++ config/_zaborona_v2/root/zaborona-vpn/iptables-custom.sh
@@ -19,7 +19,27 @@ VPN_ADDR_6=""
 #iptables -t filter -A FORWARD -i $VPN_NAME_INTERFACE --match connmark --mark 1 -j zbrnhlpvpnwhitelist
 #iptables -t filter -A FORWARD -i $VPN_NAME_INTERFACE --match connmark --mark 1 -j REJECT
 ##iptables -t filter -A FORWARD -i $WAN_4 -o $VPN_NAME_INTERFACE -j ACCEPT
-### NO FERM - If you are not using ferm, then uncomment the following block of code ### 
+#iptables -I FORWARD -o VPN_NAME_INTERFACE -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+#iptables -t nat -N dnsmap
+#iptables -t nat -A PREROUTING -s $VPNUDP_RANGE !-d $VPNUDP_DNS -p udp --dport 53 -m u32 --u32 '0x1C & 0xFFCF = 0x0100 && 0x1E & 0xFFFF = 0x0001' -j REDIRECT --to-ports 53
+#iptables -t nat -A PREROUTING -s $VPNTCP_RANGE !-d $VPNTCP_DNS -p tcp --dport 53 -m u32 --u32 '0x1C & 0xFFCF = 0x0100 && 0x1E & 0xFFFF = 0x0001' -j REDIRECT --to-ports 53
+#iptables -t nat -A PREROUTING -s $VPNUDP_RANGE -d $VPNUDP_DNS -j ACCEPT
+#iptables -t nat -A PREROUTING -s $VPNTCP_RANGE -d $VPNTCP_DNS -j ACCEPT
+#iptables -t nat -A PREROUTING -s $VPNUDP_RANGE !-d $DNSMAP_RANGE -j MARK --set-mark 1
+#iptables -t nat -A PREROUTING -s $VPNTCP_RANGE !-d $DNSMAP_RANGE -j MARK --set-mark 1
+#iptables -t nat -A PREROUTING -s $VPNUDP_RANGE -d $DNSMAP_RANGE -j dnsmap
+#iptables -t nat -A PREROUTING -s $VPNTCP_RANGE -d $DNSMAP_RANGE -j dnsmap
+#iptables -t nat -A POSTROUTING -s $VPNUDP_RANGE -j MASQUERADE
+#iptables -t nat -A POSTROUTING -s $VPNTCP_RANGE -j MASQUERADE
+
+### NO FERM - If you are not using ferm, then uncomment the following block of code ###
+
+### DROP NETWORKS ###
+### 224.0.0.0/4 (MULTICAST D)
+### 240.0.0.0/5 (E)
+iptables -I INPUT -i $WAN_4 -s 224.0.0.0/4 -j DROP
+iptables -I INPUT -i $WAN_4 -s 240.0.0.0/5 -j DROP
+### DROP NETWORKS ### 
 
 ### DROP TORRENTS ###
 iptables -I FORWARD 1 -m string --string "BitTorrent" --algo bm --to 65535 -j DROP
@@ -61,8 +81,7 @@ iptables -t mangle -A POSTROUTING -p udp -m string --algo bm --string "tracker" 
 #iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT
 #iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT
 
-#
-
+# DROP CONN PORTS #
 iptables -t mangle -I POSTROUTING -p tcp --dport 22 -j DROP
 
 iptables -t mangle -I POSTROUTING -p tcp --dport 25 -j DROP
@@ -71,20 +90,14 @@ iptables -t mangle -I POSTROUTING -p tcp --dport 587 -j DROP
 
 iptables -t mangle -I POSTROUTING -p tcp --dport 1337 -j DROP
 iptables -t mangle -I POSTROUTING -p tcp --dport 6969 -j DROP
+# DROP CONN PORTS #
 
 #
-iptables -A FORWARD -i $WAN_4 -o zaborona+ -j ACCEPT
-iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+#iptables -A FORWARD -i $WAN_4 -o zaborona+ -j ACCEPT
+#iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-iptables -t nat -A POSTROUTING -o $WAN_4 -s 192.168.0.0/16 -j MASQUERADE
-echo 1 > /proc/sys/net/ipv4/ip_forward
-
-### DROP NETWORKS ###
-### 224.0.0.0/4 (MULTICAST D)
-### 240.0.0.0/5 (E)
-iptables -I INPUT -i $WAN_4 -s 224.0.0.0/4 -j DROP
-iptables -I INPUT -i $WAN_4 -s 240.0.0.0/5 -j DROP
-### DROP NETWORKS ###
+#iptables -t nat -A POSTROUTING -o $WAN_4 -s 192.168.0.0/16 -j MASQUERADE
+#echo 1 > /proc/sys/net/ipv4/ip_forward
 
 # PREROUTING - Access For VPN-users (PPTP). All traffic
 #iptables -t nat -I POSTROUTING -p tcp -s 192.168.115.0/24 -d 0.0.0.0/0 -o $WAN_4 -j MASQUERADE
@@ -96,7 +109,7 @@ iptables -I INPUT -i $WAN_4 -s 240.0.0.0/5 -j DROP
 ############################
 #iptables -I FORWARD -i $WAN_4 -o ppp+ -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -I FORWARD -i ppp+ -o $WAN_4 -d 0.0.0.0/0 -j ACCEPT
-iptables -I FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+#iptables -I FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 # PPTP
 #iptables -t nat -I POSTROUTING -p tcp -s 192.168.216.0/22 -d 0.0.0.0/0 -o $WAN_4 -j MASQUERADE
 # L2TP-IPsec
@@ -133,10 +146,11 @@ iptables -I INPUT -i $WAN_4 -p tcp --dport 1723 -j ACCEPT
 #iptables -I FORWARD -s 192.168.15.0/24 -o $WAN_4 -d 0.0.0.0/0 -j ACCEPT
 ### INDIVIDUAL CFG ###
 
-# OUTPUT Tor
+# Tor DNS and Routes #
 iptables -t nat -I PREROUTING -p udp --dport 53 -m string --hex-string "|056f6e696f6e00|" --algo bm -j REDIRECT --to-ports 5300
 iptables -t nat -I OUTPUT -p udp --dport 53 -m string --hex-string "|056f6e696f6e00|" --algo bm -j REDIRECT --to-ports 5300
 #iptables -t nat -I PREROUTING -p tcp -d 192.168.13.0/24 -j REDIRECT --to-port 9040
 #iptables -t nat -I OUTPUT -p tcp -d 192.168.13.0/24 -j REDIRECT --to-port 9040
+# Tor DNS and Routes #
  
 exit 0
