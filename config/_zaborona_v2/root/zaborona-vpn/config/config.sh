82664ab042c1174b01cfd25a417aa02a252e54a2
Roman <toxi@ua.fm>
2023-01-30 14:29:25 +0200

    Update Site

--- config/_zaborona_v2/root/zaborona-vpn/config/config.sh
+++ config/_zaborona_v2/root/zaborona-vpn/config/config.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # HTTPS (TLS) proxy address
-PACHTTPSHOST='proxy-ssl.zaboronahelp.pp.ua:3143'
+#PACHTTPSHOST='proxy-ssl.zaboronahelp.pp.ua:3143'
+PACHTTPSHOST='socks.zaboronahelp.pp.ua:1488'
 
 # Usual proxy address
-PACPROXYHOST='proxy-nossl.zaboronahelp.pp.ua:29976' 
+#PACPROXYHOST='proxy-nossl.zaboronahelp.pp.ua:29976'
+PACPROXYHOST='socks.zaboronahelp.pp.ua:1488' 
 
 # Special proxy address for ranges
-PACPROXYSPECIAL='CCAHIHA.zaboronahelp.pp.ua:3128'
+#PACPROXYSPECIAL='CCAHIHA.zaboronahelp.pp.ua:3128'
+PACPROXYSPECIAL='socks.zaboronahelp.pp.ua:1488'
 
 PACFILE="result/proxy-host-ssl.pac"
 PACFILE_NOSSL="result/proxy-host-nossl.pac"
