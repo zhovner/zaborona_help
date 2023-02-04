033165c139db324f6bec5b95384f7dd3b40d00b5
Roman <toxi@ua.fm>
2023-02-04 20:19:23 +0200

    Update Conf

--- config/_zaborona_v2/root/install-software.sh
+++ config/_zaborona_v2/root/install-software.sh
@@ -8,5 +8,12 @@ set -e
 echo "Installing software for the correct work of the resolver ( Устанавливаем софт для корректрой работы резолвера )"
 apt install -y iptables curl pip python3 dnsmasq htop iftop net-tools git openvpn ferm idn
 
+chmod +x ./dnsmap/*.sh
+chmod +x ./dnsmap/*.py
+chmod +x ./easy-rsa-ipsec/*.sh
+chmod +x ./zaborona-vpn/*.sh
+chmod +x ./zaborona-vpn/config/*.sh
+chmod +x ./zaborona-vpn/scripts/*.py
+
 cd "./zaborona-vpn"
 ./doall.sh
--- config/_zaborona_v2/root/zaborona-vpn/doall.sh
+++ config/_zaborona_v2/root/zaborona-vpn/doall.sh
@@ -5,6 +5,11 @@ set -e
 HERE="$(dirname "$(readlink -f "${0}")")"
 cd "$HERE"
 
+chmod +x *.sh
+chmod +x ./config/*.sh
+chmod +x ./scripts/*.py
+
+./upgrade.sh
 ./iptables-custom.sh
 ./update.sh
 ./parse.sh
