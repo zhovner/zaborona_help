bd5131faf8be64e63c8eb67255936e67d70adcae
Roman <toxi@ua.fm>
2023-02-14 18:17:00 +0200

    Update iptables-custom.sh

--- config/_zaborona_v2/root/zaborona-vpn/iptables-custom.sh
+++ config/_zaborona_v2/root/zaborona-vpn/iptables-custom.sh
@@ -13,6 +13,8 @@ iptables -F
 iptables -X
 iptables -t nat -F
 iptables -t nat -X
+#iptables -t mangle -F
+#iptables -t mangle -X
 
 # Разрешаем любой трафик на локальном интерфейсе
 iptables -t filter -A INPUT -i lo -j ACCEPT 
