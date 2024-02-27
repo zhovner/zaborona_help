ac5fa4107b7d5d809978019970282cff8791eba1
Roman <toxi@ua.fm>
2024-02-27 04:01:13 +0200

    Update Config Zaborona_v2
    - Добавлены новые сайты через ДНС
    - Обновлены конфиги Zaborona_v2
    - Изменена структура обновления конфигов, чтобы основные конфиги считывались из одного-двух файлов, а скрипты на сервере будут парсить файл(ы) и автоматически создавать нужные файлы конфигурации для работающих служб в нужном формате. Раньше эта процедура делалась вручную, что занимало много времени, так как нужно было открыть 3-4 конфига в разных каталогах и вставить одинаковые данные в них. Автоматизация создания конфига с маршрутами для OpenVPN - в процессе.
    - Исправлены мелкие ошибки в текущих конфигах. На работу не повлияет.
    - Создана база данных со списком доменов, с которой будет взаимодействовать бот и который будет автоматически вносить в базу новые домены и/или ip адреса для добавления их в список разрешенных, чтобы еще больше автоматизировать процесс добавления доменов и чтобы пользователи в комментариях не ждали по несколько дней/недель добавления домена в список обхода блокировок (премодерация будет, но в каком виде и по каки критериям - на стадии реализации). С автоматическим добавлением IP адресов - пока на стадии реализации. Скорее всего список будет обновляться автоматически с ресурса https://ipv4.fetus.jp/ru.txt для конфига big_routes, и возможно, некоторые ip-адреса будут добавляться по запросу в конфиг big_routes.
    - Реализация самого бота, который будет взаимодействовать с базой данных. На стадии разработки.

--- config/updateCFGzaborona.sh
+++ config/updateCFGzaborona.sh
@@ -4,22 +4,26 @@ set -e
 HERE="$(dirname "$(readlink -f "${0}")")"
 cd "$HERE"
 
-LISTLINK_ALLCONFIG="https:/raw.githubusercontent.com/zhovner/zaborona_help/master/config/_zaborona_v2/etc/dnsmasq.d/zaborona-dns-resovler"
+#LISTLINK_ALLCONFIG="https:/raw.githubusercontent.com/zhovner/zaborona_help/master/config/_zaborona_v2/etc/dnsmasq.d/zaborona-dns-resovler"
+LISTLINK_ALLCONFIG="https:/raw.githubusercontent.com/zhovner/zaborona_help/master/config/domainsdb.txt"
+#LISTLINK_ALLCONFIG2="https:/zaborona.help/domainsdb.txt"
 
 WORKFOLDERNAME="/tmp"
+#WORKFOLDERNAME=$PWD/result
 WORKFOLDERNAME2="/etc/dnsmasq.d"
 FILENAMERESULT="zaborona-dns-resovler-tmp1"
 FILENAMERESULT2="zaborona-dns-resovler-tmp2"
 FILENAMERESULT3="zaborona-dns-resovler"
+FILENAMERESULT4="domainsdb.txt"
 
 #curl -f --fail-early --compressed --connect-timeout 15 -o $WORKFOLDERNAME/$FILENAMERESULT3 "$LISTLINK_ALLCONFIG_DONE"
 
 #FILENAMEALLCONFIG_INCLUDE1=
 
 #for FILENAMEALLCONFIG_INCLUDE1 in $LISTLINK_ALLCONFIG; do
-	touch $WORKFOLDERNAME/$FILENAMERESULT3
+#	touch $WORKFOLDERNAME/$FILENAMERESULT3
 #	LISTLINK_ALLCONFIG_DONE=$LISTLINK_ALLCONFIG/$FILENAMEALLCONFIG_INCLUDE1
-	curl -f --fail-early --compressed --connect-timeout 15 -o $WORKFOLDERNAME/$FILENAMERESULT3 "$LISTLINK_ALLCONFIG" &&
+	curl -f --fail-early --compressed --connect-timeout 15 -o $WORKFOLDERNAME/$FILENAMERESULT4 "$LISTLINK_ALLCONFIG" &&
 LISTSIZE="$(curl -sI --connect-timeout 15 "$LISTLINK_ALLCONFIG" | awk 'BEGIN {IGNORECASE=1;} /content-length/ {sub(/[ \t\r\n]+$/, "", $2); print $2}')"
 [[ "$LISTSIZE" == "$(stat -c '%s' $WORKFOLDERNAME2/$FILENAMERESULT3)" ]] && echo "The files are the same ($WORKFOLDERNAME2/$FILENAMERESULT3)" && exit 2
 #sort -u $WORKFOLDERNAME/$FILENAMEALLCONFIG_INCLUDE1 >> $WORKFOLDERNAME/$FILENAMERESULT2
@@ -27,6 +31,16 @@ LISTSIZE="$(curl -sI --connect-timeout 15 "$LISTLINK_ALLCONFIG" | awk 'BEGIN {IG
 
 # Собираем все в один файл, чтобы за один раз прогнать все записи и не плодить много парсинга
 #echo "Собираем все в один файл, чтобы за один раз прогнать все записи и не плодить много парсинга"
+
+# Generate dnsmasq aliases
+echo -n > $WORKFOLDERNAME/$FILENAMERESULT3
+while read -r line
+do
+	# convert an IDN to Punycode
+	PunycodeConvert="$(echo $line | idn)"
+    echo "server=/$PunycodeConvert/127.0.0.4#5959" >> $WORKFOLDERNAME/$FILENAMERESULT3
+done < $WORKFOLDERNAME/$FILENAMERESULT4
+
 echo "Update File"
 #sort -u $WORKFOLDERNAME/$FILENAMERESULT_TMP1 $WORKFOLDERNAME/$FILENAMERESULT_TMP2 $WORKFOLDERNAME/$FILENAMERESULT_TMP4 > $WORKFOLDERNAME/$FILENAMERESULT
 mv $WORKFOLDERNAME/$FILENAMERESULT3 $WORKFOLDERNAME2/$FILENAMERESULT3
@@ -36,6 +50,6 @@ echo "Update File OK"
 sleep 2
 
 echo "DNSMASQ Restart"
-systemctl restart dnsmasq
+systemctl reload dnsmasq
 
 exit 0
--- config/updateCFGzaboronaIPTablesFREM-ipv6.sh
+++ config/updateCFGzaboronaIPTablesFREM-ipv6.sh
@@ -4,29 +4,50 @@ set -e
 HERE="$(dirname "$(readlink -f "${0}")")"
 cd "$HERE"
 
-LISTLINK_ALLCONFIG="https:/raw.githubusercontent.com/zhovner/zaborona_help/master/config/ferm/whitelist_ipv6.conf"
+#LISTLINK_ALLCONFIG="https:/raw.githubusercontent.com/zhovner/zaborona_help/master/config/ferm/whitelist_ipv6.conf"
+LISTLINK_ALLCONFIG="https:/raw.githubusercontent.com/zhovner/zaborona_help/master/config/ipsdb_v6.txt"
 
 WORKFOLDERNAME="/tmp"
 WORKFOLDERNAME2="/etc/ferm"
+WORKFOLDERNAME3=$PWD
 FILENAMERESULT="zaborona-dns-resovler-tmp1"
 FILENAMERESULT2="zaborona-dns-resovler-tmp2"
 FILENAMERESULT3="whitelist_ipv6.conf"
+FILENAMERESULT4="ipsdbFERM_v6.txt"
 
 #curl -f --fail-early --compressed --connect-timeout 15 -o $WORKFOLDERNAME/$FILENAMERESULT3 "$LISTLINK_ALLCONFIG_DONE"
 
 #FILENAMEALLCONFIG_INCLUDE1=
 
 #for FILENAMEALLCONFIG_INCLUDE1 in $LISTLINK_ALLCONFIG; do
-	touch $WORKFOLDERNAME/$FILENAMERESULT3
-#	LISTLINK_ALLCONFIG_DONE=$LISTLINK_ALLCONFIG/$FILENAMEALLCONFIG_INCLUDE1
-	curl -f --fail-early --compressed --connect-timeout 15 -o $WORKFOLDERNAME/$FILENAMERESULT3 "$LISTLINK_ALLCONFIG" &&
+#	touch $WORKFOLDERNAME/$FILENAMERESULT3
+##	LISTLINK_ALLCONFIG_DONE=$LISTLINK_ALLCONFIG/$FILENAMEALLCONFIG_INCLUDE1
+#	curl -f --fail-early --compressed --connect-timeout 15 -o $WORKFOLDERNAME/$FILENAMERESULT3 "$LISTLINK_ALLCONFIG" &&
+#LISTSIZE="$(curl -sI --connect-timeout 15 "$LISTLINK_ALLCONFIG" | awk 'BEGIN {IGNORECASE=1;} /content-length/ {sub(/[ \t\r\n]+$/, "", $2); print $2}')"
+#[[ "$LISTSIZE" == "$(stat -c '%s' $WORKFOLDERNAME2/$FILENAMERESULT3)" ]] && echo "The files are the same ($WORKFOLDERNAME2/$FILENAMERESULT3)" && exit 2
+
+	curl -f --fail-early --compressed --connect-timeout 15 -o $WORKFOLDERNAME/$FILENAMERESULT4 "$LISTLINK_ALLCONFIG" &&
 LISTSIZE="$(curl -sI --connect-timeout 15 "$LISTLINK_ALLCONFIG" | awk 'BEGIN {IGNORECASE=1;} /content-length/ {sub(/[ \t\r\n]+$/, "", $2); print $2}')"
 [[ "$LISTSIZE" == "$(stat -c '%s' $WORKFOLDERNAME2/$FILENAMERESULT3)" ]] && echo "The files are the same ($WORKFOLDERNAME2/$FILENAMERESULT3)" && exit 2
+
 #sort -u $WORKFOLDERNAME/$FILENAMEALLCONFIG_INCLUDE1 >> $WORKFOLDERNAME/$FILENAMERESULT2
 #done
 
 # Собираем все в один файл, чтобы за один раз прогнать все записи и не плодить много парсинга
 #echo "Собираем все в один файл, чтобы за один раз прогнать все записи и не плодить много парсинга"
+
+	# Generate ferm file - IP ACCEPT
+	cp $WORKFOLDERNAME3/updateCFGzaboronaIPTablesFREM-ipv6_header.txt $WORKFOLDERNAME/$FILENAMERESULT3
+	
+	while read -r line
+	do
+		#iptables -w -A ZABORONA_V4 -d "$line" -j ACCEPT
+		echo "$line" >> $WORKFOLDERNAME/$FILENAMERESULT3
+	
+	done < $WORKFOLDERNAME/$FILENAMERESULT4
+	
+	echo ");\n" >> $WORKFOLDERNAME/$FILENAMERESULT3
+
 echo "Update File"
 #sort -u $WORKFOLDERNAME/$FILENAMERESULT_TMP1 $WORKFOLDERNAME/$FILENAMERESULT_TMP2 $WORKFOLDERNAME/$FILENAMERESULT_TMP4 > $WORKFOLDERNAME/$FILENAMERESULT
 mv $WORKFOLDERNAME/$FILENAMERESULT3 $WORKFOLDERNAME2/$FILENAMERESULT3
