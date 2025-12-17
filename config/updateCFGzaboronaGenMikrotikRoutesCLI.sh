fe215bb461453c6c96a0b5919aa26345faaff8e6
Roman <toxi@ua.fm>
2025-12-17 02:30:09 +0200

    Create updateCFGzaboronaGenMikrotikRoutesCLI.sh

---
+++ config/updateCFGzaboronaGenMikrotikRoutesCLI.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Show Errors And Stop Script
+#set -e
+
+HERE="$(dirname "$(readlink -f "${0}")")"
+cd "$HERE"
+
+# Generate New route file for Mikrotik
+echo -n > ./mikrotik-add-ranges.txt
+echo -n > ./mikrotik-del-ranges.txt
+
+#cat ./updateCFGzaboronaOpenVPNRoutesBIG_header0.txt > ./openvpn-blocked-ranges.txt
+
+while read -r line
+do
+	# Если в файле есть мусор (пробелы, комментарии)
+	#line="$(echo "$line" | xargs)"
+    #[[ -z "$line" || "$line" =~ ^# ]] && continue
+    echo "/ip route add dst-address=$line gateway=ovpn-out1" >> ./mikrotik-add-ranges_gen.txt
+    echo "/ip route remove [find dst-address=$line gateway=ovpn-out1]" >> ./mikrotik-del-ranges_gen.txt
+	# Массовое удаление (осторожно)
+    #echo "/ip route remove [find dst-address=$line]" >> ./mikrotik-del-ranges.txt
+	# Массовое удаление (осторожно)
+	#echo "/ip route remove [find gateway=ovpn-out1]" >> ./mikrotik-del-ranges.txt
+done < ./ipsdb0.txt
+
+#sort -u ./openvpn-blocked-ranges.txt ./ccd_zaborona_big_routes_DEFAULT.txt > ./openvpn-blocked-ranges_sorted.txt
+sort -u ./mikrotik-add-ranges_gen.txt > ./mikrotik-add-ranges.txt
+sort -u ./mikrotik-del-ranges_gen.txt > ./mikrotik-del-ranges.txt
+
+#cat ./updateCFGzaboronaOpenVPNRoutesBIG_header0.txt >> ./openvpn-blocked-ranges_sorted.txt
+
+#mv ./openvpn-blocked-ranges_sorted.txt /etc/openvpn/ccd_zaborona_big_routes/DEFAULT
+
+echo "Update File OK"
+
+exit 0
